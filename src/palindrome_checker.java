c8b3d8cd9e9f26a892832967905985291b5e22ff
vijishnu06 <jishnun06@gmail.com>
2026-03-10 15:44:37 +0530

    UC2:  Print a Hardcoded Palindrome Result

--- src/palindrome_checker.java
+++ src/palindrome_checker.java
@@ -1,6 +1,18 @@
 public class palindrome_checker {
     public static void main(String[] args) {
 
-        System.out.print("Welcome to the Palindrome Checker Manangement System");
+        String word = "madam";
+
+        String reversed = "";
+
+        for (int i = word.length() - 1; i >= 0; i--) {
+            reversed += word.charAt(i);
+        }
+
+        if (word.equals(reversed)) {
+            System.out.println(word + " is a Palindrome");
+        } else {
+            System.out.println(word + " is NOT a Palindrome");
+        }
     }
 }
