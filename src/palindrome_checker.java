1e841220d49cbf9acbfc92ba5498a1b0d9515e69
vijishnu06 <jishnun06@gmail.com>
2026-03-17 13:55:39 +0530

    UC9: Booking History & Reporting

--- src/palindrome_checker.java
+++ src/palindrome_checker.java
@@ -1,36 +1,35 @@
-import java.util.LinkedList;
-
 public class palindrome_checker {
+
     public static void main(String[] args) {
 
-
-        String input = "level";
-
+        String input = "madam";
 
         if (input == null) {
             System.out.println("Input is null");
             return;
         }
 
-
-        LinkedList<Character> list = new LinkedList<>();
-
-
-        for (char c : input.toCharArray()) {
-            list.add(c);
-        }
-
-        boolean isPalindrome = true;
-
-
-        while (list.size() > 1) {
-            if (!list.removeFirst().equals(list.removeLast())) {
-                isPalindrome = false;
-                break;
-            }
-        }
+        boolean result = check(input, 0, input.length() - 1);
 
         System.out.println("Input : " + input);
-        System.out.println("Is Palindrome? : " + isPalindrome);
+        System.out.println("Is Palindrome? : " + result);
+    }
+
+    // Recursive method
+    private static boolean check(String s, int start, int end) {
+
+        // Base condition: 0 or 1 character left
+        if (start >= end) {
+            return true;
+        }
+
+        // If mismatch, stop immediately
+        if (s.charAt(start) != s.charAt(end)) {
+            return false;
+        }
+
+        // Recursive call for inner substring
+        return check(s, start + 1, end - 1);
     }
 }
+
